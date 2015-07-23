3d67539446812c555d36a21581330d911ae93150
wangjiajun <wangjiajun@58.com>
2015-07-23 15:03:54 +0800

    dedup

--- src/main/java/com/jerry/hadoop/mapreducer/dedup/MapperClass.java
+++ src/main/java/com/jerry/hadoop/mapreducer/dedup/MapperClass.java
@@ -6,11 +6,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class MapperClass extends Mapper<Object, Text, Text, Text>{
-	private static Text line=new Text();//每行数据
 	@Override
-	protected void map(Object key, Text value,Context context)
+	protected void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
-		line = value;
-		context.write(line, new Text(""));
+		context.write(value, new Text(""));
 	}
 }
