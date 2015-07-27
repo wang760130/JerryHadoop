af317b3161ceda7eca9a9d63a14b3329e4fd000c
wangjiajun <wangjiajun@58.com>
2015-07-27 20:54:25 +0800

    canopy

---
+++ src/main/java/com/jerry/mahout/clustering/canopy/VectorWritable.java
@@ -0,0 +1,18 @@
+package com.jerry.mahout.clustering.canopy;
+
+import org.apache.mahout.common.AbstractJob;
+
+/**
+ * transform text data to vectorWritable data
+ * @author Wangjiajun 
+ * @Email  wangjiajun@58.com
+ * @date   2015年7月27日
+ */
+public class VectorWritable extends AbstractJob {
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		return 0;
+	}
+
+}
